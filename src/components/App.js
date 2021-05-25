d49655ce21df95560c7850fab94af69886d4a44e
geekchourasia <engineer.chourasia@gmail.com>
2021-05-25 14:24:47 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,59 @@
 import React, { Component, useState } from "react";
-import '../styles/App.css';
+
+//import '../styles/App.css';
+
 class App extends Component {
 
 	constructor(props) {
-		super(props);
-		this.state = ({ isClicked: false });
 
-		// this.state = this.printPara.bind(this);
+		super(props);
+
+		this.state = { isClicked: false };
+
+		//this.printPara = this.printPara.bind(this);
+
 	}
 
 	printPara = () => {
-		//- declare directly don't use const or let
 
-		this.setState({ isClicked: true })
-	};
+		this.setState({ isClicked: true });
+
+	}
 
 	render() {
+
 		return (
+
 			<div id="main">
-				<button id="click" onClick={this.printPara}>Click Me</button>
-				{this.state.isClicked ?<para id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</para>: null}
+
+				<button id="click" onClick={this.printPara}>
+
+					Click Me
+
+        	</button>
+
+				{this.state.isClicked ? (
+
+					<para>
+
+						Hello, I've learnt to use the full-stack evaluation tool. This makes
+
+						me so happy
+
+					</para>
+
+				) : null}
+
 			</div>
 
 		);
+
 	}
 
 }
 
 
 export default App;
-
 
 
 
